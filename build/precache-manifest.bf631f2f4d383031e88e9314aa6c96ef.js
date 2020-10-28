06270d2f38f6844aa35a366bfac950bb7a86030b
Frankie Tse <tsezg523@gmail.com>
2020-10-28 14:27:04 +0800

    Remove /build from .gitignore

---
+++ build/precache-manifest.bf631f2f4d383031e88e9314aa6c96ef.js
@@ -0,0 +1,134 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a39bee7acce46b1aadac50f425f461db",
+    "url": "/portfolio-website/index.html"
+  },
+  {
+    "revision": "7582b281ca4014552e60",
+    "url": "/portfolio-website/static/css/3.00373544.chunk.css"
+  },
+  {
+    "revision": "f0630242198187fc7478",
+    "url": "/portfolio-website/static/css/4.a922a401.chunk.css"
+  },
+  {
+    "revision": "8b31fc424e3aacfaec85",
+    "url": "/portfolio-website/static/css/main.89231dc0.chunk.css"
+  },
+  {
+    "revision": "cc2bf91576ee51198cbd",
+    "url": "/portfolio-website/static/js/2.d47f9006.chunk.js"
+  },
+  {
+    "revision": "4db75b29a1b6c7d48b1118aa82729e2a",
+    "url": "/portfolio-website/static/js/2.d47f9006.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7582b281ca4014552e60",
+    "url": "/portfolio-website/static/js/3.0e787a14.chunk.js"
+  },
+  {
+    "revision": "f0630242198187fc7478",
+    "url": "/portfolio-website/static/js/4.b67b84ee.chunk.js"
+  },
+  {
+    "revision": "8b31fc424e3aacfaec85",
+    "url": "/portfolio-website/static/js/main.f3eb68f4.chunk.js"
+  },
+  {
+    "revision": "c63ef59eabc743cb5617",
+    "url": "/portfolio-website/static/js/runtime-main.78605b65.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/portfolio-website/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/portfolio-website/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "47a7a717679b640ceb1b05d38c480a61",
+    "url": "/portfolio-website/static/media/codeInLogo.47a7a717.webp"
+  },
+  {
+    "revision": "68e841388612be0c5143e2932fb77d4f",
+    "url": "/portfolio-website/static/media/contactMail.68e84138.webp"
+  },
+  {
+    "revision": "51cf7192327488da3d2a171280680c16",
+    "url": "/portfolio-website/static/media/contactMailDark.51cf7192.svg"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/portfolio-website/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "8ddf76a14a2e3ad3ba62b46d49a75a74",
+    "url": "/portfolio-website/static/media/facebookLogo.8ddf76a1.png"
+  },
+  {
+    "revision": "c24ba79111a90758a3a472a906ab2901",
+    "url": "/portfolio-website/static/media/googleAssistant.c24ba791.svg"
+  },
+  {
+    "revision": "e79c639294c805688be731921368c8f8",
+    "url": "/portfolio-website/static/media/googleAssistantLogo.e79c6392.webp"
+  },
+  {
+    "revision": "7c7f8f8963f9c2c545333cf7f26ef2f1",
+    "url": "/portfolio-website/static/media/hkustLogo.7c7f8f89.png"
+  },
+  {
+    "revision": "9b2df1596555b0b39e53abfe049adf06",
+    "url": "/portfolio-website/static/media/ibmLogo.9b2df159.png"
+  },
+  {
+    "revision": "b05ae5b575a7e1125610944dc152c35f",
+    "url": "/portfolio-website/static/media/jsFramework.b05ae5b5.svg"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/portfolio-website/static/media/logo.25bf045c.svg"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/portfolio-website/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "829ad0a1deef4d7295adfecdf12df2be",
+    "url": "/portfolio-website/static/media/nextuLogo.829ad0a1.webp"
+  },
+  {
+    "revision": "c1a95f7335cbfb730b937e19e8830a53",
+    "url": "/portfolio-website/static/media/programmer.c1a95f73.svg"
+  },
+  {
+    "revision": "1a60e8a9e2c2d6a539c7692f27fe6790",
+    "url": "/portfolio-website/static/media/pwa.1a60e8a9.webp"
+  },
+  {
+    "revision": "ee62cb58630f29a6e6201b49d4c03728",
+    "url": "/portfolio-website/static/media/pwaLogo.ee62cb58.webp"
+  },
+  {
+    "revision": "25018d29d5ab115bc51c3e9a2d92f90f",
+    "url": "/portfolio-website/static/media/saayaHealthLogo.25018d29.webp"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/portfolio-website/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "13780cdd0144cacdef25486e2c2503f8",
+    "url": "/portfolio-website/static/media/talksCardBack.13780cdd.svg"
+  },
+  {
+    "revision": "b48e86c9a956d3a3a32ca4a5ae05ddc7",
+    "url": "/portfolio-website/static/media/talksCardBackAlt.b48e86c9.svg"
+  },
+  {
+    "revision": "d7eedc22930463a18c5129543f55e409",
+    "url": "/portfolio-website/static/media/telelinkLogo.d7eedc22.png"
+  }
+]);
